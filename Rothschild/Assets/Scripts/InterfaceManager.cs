888e7e792e5a02962dcf9971cc977c696b7306d8
MaiJC <maijiancheng@qq.com>
2018-09-09 20:07:23 +0800

    add the button color change code

---
+++ Rothschild/Assets/Scripts/InterfaceManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceManager : MonoBehaviour {
+
+    public GameObject myEvent;
+
+    private ArrayList currentEventList = new ArrayList();
+
+	// Use this for initialization
+	void Start () {
+        //GameObject tmp = (GameObject)Instantiate(myEvent, new Vector3(0, 0, 0), Quaternion.identity);
+        //GameObject canvas = GameObject.Find("Canvas");
+        //tmp.transform.SetParent(canvas.transform);
+
+        //currentEventList.Add(tmp);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Rothschild/Assets/Scripts/OnColorChange.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OnColorChange : EventTrigger {
+
+    private Graphic targetGraphic;
+    private bool isSelected = false;
+
+    private Color normalColor = Color.white;
+    private Color clickColor = Color.black;
+    private Color enterColor = Color.grey;
+    private Color selectColor = Color.red;
+
+	// Use this for initialization
+	void Start () {
+        targetGraphic = this.GetComponent<Button>().targetGraphic;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        //base.OnPointerEnter(eventData);
+        targetGraphic.color = enterColor;
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        //base.OnPointerExit(eventData);
+        if (isSelected)
+            targetGraphic.color = selectColor;
+        else
+            targetGraphic.color = normalColor;
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        targetGraphic.color = clickColor;
+        isSelected = !isSelected;
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        if (isSelected)
+            targetGraphic.color = selectColor;
+        else
+            targetGraphic.color = normalColor;
+    }
+}
