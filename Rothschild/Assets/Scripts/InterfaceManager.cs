cb99f4effa3ea84413f0ec0d43eaf6a844b62d70
MaiJC <maijiancheng@qq.com>
2018-09-11 21:48:50 +0800

    增加了读取后台数据的代码

--- Rothschild/Assets/Scripts/InterfaceManager.cs
+++ Rothschild/Assets/Scripts/InterfaceManager.cs
@@ -3,40 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InterfaceManager : MonoBehaviour {
+public class InterfaceManager : MonoBehaviour
+{
 
-    private Image imageOfConvesOne;
     private List<string> cardPath = new List<string>();
-    private int currentIndex;
+    private List<int> commonEventID = new List<int>();
+    private int levelCount;
+    private List<List<int>> levelEventID = new List<List<int>>();
+    private List<List<int>> levelStoryID = new List<List<int>>();
+    private int currentEventID;
+    private int currentLevel;
+    private Dictionary<int, string> eventUIPath = new Dictionary<int, string>();
+    private Dictionary<int, string> eventText = new Dictionary<int, string>();
+
+
+    private List<Image> personImage = new List<Image>();
+    private List<OnCardClicked> person = new List<OnCardClicked>();
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
         cardPath.Add("banana");
         cardPath.Add("monkey");
         cardPath.Add("banana_alfa");
-        currentIndex = Random.Range(0, 3);
 
-        imageOfConvesOne = GameObject.Find("imageForOneEvent").GetComponent<Image>();
+        personImage.Add(GameObject.Find("PersonImageA").GetComponent<Image>());
+        personImage.Add(GameObject.Find("PersonImageB").GetComponent<Image>());
+        personImage.Add(GameObject.Find("PersonImageC").GetComponent<Image>());
+        personImage.Add(GameObject.Find("PersonImageD").GetComponent<Image>());
 
-        imageOfConvesOne.sprite = Resources.Load(cardPath[currentIndex], typeof(Sprite)) as Sprite;
+        person.Add(GameObject.Find("PersonPanelA").GetComponent<OnCardClicked>());
+        person.Add(GameObject.Find("PersonPanelB").GetComponent<OnCardClicked>());
+        person.Add(GameObject.Find("PersonPanelC").GetComponent<OnCardClicked>());
+        person.Add(GameObject.Find("PersonPanelD").GetComponent<OnCardClicked>());
+
+        foreach (Image i in personImage)
+        {
+            i.overrideSprite = Resources.Load("monkey", typeof(Sprite)) as Sprite;
+        }
+
+        Initialize();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
 
     }
 
     public void OnConfirm()
     {
-        int i = Random.Range(0, 3);
-        while(i==currentIndex)
+
+    }
+
+    void Initialize()
+    {
+        LoadRes loadRes = GameObject.Find("DataHandler").GetComponent<LoadRes>();
+
+        levelCount = loadRes.GetLevelCount();
+        commonEventID = loadRes.GetCommonEventID();
+        for (int i = 1 ; i <= levelCount; i++)
         {
-            i = Random.Range(0, 3);
+            levelEventID.Add(loadRes.GetLevelEventID(i));
+            levelStoryID.Add(loadRes.GetLevelStoryID(i));
         }
-        currentIndex = i;
-        imageOfConvesOne.sprite = Resources.Load(cardPath[i], typeof(Sprite)) as Sprite;
-        Debug.Log(currentIndex);
+        for (int i = 0; i < commonEventID.Count; i++)
+        {
+            int eventID = commonEventID[i];
+            string path = loadRes.GetEventUIPath(eventID);
+            string text = loadRes.GetEventText(eventID);
+            eventUIPath.Add(eventID, path);
+            eventText.Add(eventID, text);
+        }
+        for (int i = 0; i < levelCount; i++)
+        {
+            for (int j = 0; j < levelEventID[i].Count; j++)
+            {
+                int eventID = levelEventID[i][j];
+                string path = loadRes.GetEventUIPath(eventID);
+                string text = loadRes.GetEventText(eventID);
+                eventUIPath.Add(eventID, path);
+                eventText.Add(eventID, text);
+            }
+        }
     }
 
 }
