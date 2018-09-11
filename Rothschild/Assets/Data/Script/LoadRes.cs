4b03da0b01f636f33ea423d63fbc4948d9401927
xkchen1992 <1554657616@qq.com>
2018-09-11 16:52:31 +0800

    添加数据表的处理和玩家注册登录

---
+++ Rothschild/Assets/Data/Script/LoadRes.cs
@@ -0,0 +1,241 @@
+﻿using Mono.Xml;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Security;
+using System.Xml;
+using UnityEngine;
+
+public class LoadRes : MonoBehaviour {
+
+    private XmlNode eventRootNode;
+    private XmlNode uiResRootNode;
+    private XmlNode storyEventRootNode;
+    private XmlNode storyLevelRootNode;
+
+ //   private int storySeq = 0;
+
+    void LoadXml()
+    {
+        // 加载事件表
+        string eventPath = Application.dataPath + "/Data/Xml/event.xml";
+        XmlDocument eventDoc = new XmlDocument();
+        eventDoc.Load(eventPath);
+        eventRootNode = eventDoc.SelectSingleNode("TEventTable_Tab");
+
+        // 加载UI资源表
+        string uiResPath = Application.dataPath + "/Data/Xml/ui_res.xml";
+        XmlDocument uiResDoc = new XmlDocument();
+        uiResDoc.Load(uiResPath);
+        uiResRootNode = uiResDoc.SelectSingleNode("TUIResTable_Tab");
+
+        // 加载故事事件表
+        string storyEventPath = Application.dataPath + "/Data/Xml/story_event.xml";
+        XmlDocument storyEventDoc = new XmlDocument();
+        storyEventDoc.Load(storyEventPath);
+        storyEventRootNode = storyEventDoc.SelectSingleNode("TStoryEventTable_Tab");
+
+        // 加载故事关卡表
+        string storyLevelPath = Application.dataPath + "/Data/Xml/story_level.xml";
+        XmlDocument storyLevelDoc = new XmlDocument();
+        storyLevelDoc.Load(storyLevelPath);
+        storyLevelRootNode = storyLevelDoc.SelectSingleNode("TStoryLevelTable_Tab");
+
+    }
+
+    void PrintEventTable()
+    {
+        print("\n --------Event Table--------\n");
+
+        foreach (XmlElement item in eventRootNode)
+        {
+            int id = int.Parse(item.ChildNodes[0].InnerText);
+            int type = int.Parse(item.ChildNodes[1].InnerText);
+            int key1 = int.Parse(item.ChildNodes[2].InnerText);
+            int key2 = int.Parse(item.ChildNodes[3].InnerText);
+            int level = int.Parse(item.ChildNodes[4].InnerText);
+            int withkeyMoney = int.Parse(item.ChildNodes[5].InnerText);
+            int withkeyReputation = int.Parse(item.ChildNodes[6].InnerText);
+            int withkeyTeamwork = int.Parse(item.ChildNodes[7].InnerText);
+            int withkeyCd = int.Parse(item.ChildNodes[8].InnerText);
+            int withoutkeyMoney = int.Parse(item.ChildNodes[9].InnerText);
+            int withoutkeyReputation = int.Parse(item.ChildNodes[10].InnerText);
+            int withoutkeyTeamwork = int.Parse(item.ChildNodes[11].InnerText);
+            int withoutkeyCd = int.Parse(item.ChildNodes[12].InnerText);
+            print("ID: " + id + " Type: " + type + " Key1: " + key1 + " Key2: " + key2 + " Level: " + level +
+                " withkeyMoney: " + withkeyMoney + " WithKeyReputation: " + withkeyReputation + " WithKeyTeamwork: " + withkeyTeamwork +
+                " WithKeyCd: " + withkeyCd + " WithoutKeyMoney: " + withoutkeyMoney + " WithoutKeyReputation: " + withoutkeyReputation +
+                " WithoutKeyTeamwork: " + withoutkeyTeamwork + " WithoutKeyCd: " + withoutkeyCd);
+        }
+    }
+
+    List<int> GetCommonEventID()
+    {
+        List<int> comEventList = new List<int>();
+        foreach (XmlElement item in eventRootNode)
+        {
+            int id = int.Parse(item.ChildNodes[0].InnerText);
+            int type = int.Parse(item.ChildNodes[1].InnerText);
+            if (0 == type)
+            {
+                comEventList.Add(id);
+            }
+        }
+        return comEventList;
+    }
+
+    int GetLevelCount()
+    {
+        int maxLevel = 0;
+        foreach (XmlElement item in eventRootNode)
+        {
+            int level = int.Parse(item.ChildNodes[4].InnerText);
+            if (level > maxLevel)
+            {
+                maxLevel = level;
+            }
+        }
+        return maxLevel;
+    }
+
+    List<int> GetLevelEventID(int levelIndex)
+    {
+        List<int> levelEventList = new List<int>();
+        foreach (XmlElement item in eventRootNode)
+        {
+            int id = int.Parse(item.ChildNodes[0].InnerText);
+            int level = int.Parse(item.ChildNodes[4].InnerText);
+            if (level == levelIndex)
+            {
+                levelEventList.Add(id);
+            }
+        }
+        return levelEventList;
+    }
+
+    List<int> GetLevelStoryID(int levelIndex)
+    {
+        List<int> levelStoryList = new List<int>();
+        foreach (XmlElement item in storyLevelRootNode)
+        {
+            int storyID = int.Parse(item.ChildNodes[0].InnerText);
+            int level = int.Parse(item.ChildNodes[1].InnerText);
+            if (level == levelIndex)
+            {
+                levelStoryList.Add(storyID);
+            }
+        }
+        return levelStoryList;
+    }
+
+    List<int> GetNextStoryID(int storyID, int storySeq, int fatherEventID)
+    {
+        List<int> nextStoryList = new List<int>();
+        foreach (XmlElement item in storyEventRootNode)
+        {
+            int id = int.Parse(item.ChildNodes[0].InnerText);
+            int seq = int.Parse(item.ChildNodes[1].InnerText);
+            int before = int.Parse(item.ChildNodes[2].InnerText);
+            int later = int.Parse(item.ChildNodes[3].InnerText);
+            if (id == storyID && before == fatherEventID && storySeq == seq)
+            {
+                nextStoryList.Add(later);
+            }
+        }
+    //    ++storySeq;
+        return nextStoryList;
+    }
+
+    string GetEventUIPath(int eventID)
+    {
+        string uiPath = "";
+        foreach (XmlElement item in uiResRootNode)
+        {
+            int id = int.Parse(item.ChildNodes[0].InnerText);
+            uiPath = item.ChildNodes[1].InnerText;
+            if (id == eventID)
+            {
+                break;
+            }
+        }
+        print("eventID: " + eventID + ", Picture: " + uiPath); 
+        return uiPath;
+    }
+
+    string GetEventText(int eventID)
+    {
+        string text = "";
+        foreach (XmlElement item in uiResRootNode)
+        {
+            int id = int.Parse(item.ChildNodes[0].InnerText);
+            text = item.ChildNodes[2].InnerText;
+            if (id == eventID)
+            {
+                break;
+            }
+        }
+
+        print("eventID: " + eventID + ", Text: " + text);
+
+        return text;
+    }
+    // Use this for initialization
+    void Start () {
+
+        LoadXml();
+/*
+        PrintLevelCount();
+        PrintLevelStoryID(1);
+        PrintLevelStoryID(2);
+
+        PrintNextStoryID(1, 0, 0);
+        PrintNextStoryID(1, 1, 1001);
+        PrintNextStoryID(1, 2, 1002);
+        PrintNextStoryID(1, 2, 1003);
+        PrintNextStoryID(1, 3, 1004);
+        print(GetEventUIPath(1004));
+        print(GetEventText(1004)); 
+*/
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PrintLevelCount()
+    {
+        print("\nLevel Cout: " + GetLevelCount() + "\n");
+    }
+
+    void PrintLevelEvent(int levelIndex)
+    {
+        print("\n-------Level " + levelIndex + " Event------\n");
+        List<int> level1EventList = GetLevelEventID(levelIndex);
+        foreach (int eventID in level1EventList)
+        {
+            print(eventID);
+        }
+    }
+
+    void PrintLevelStoryID(int levelIndex)
+    {
+        print("\n-------Level " + levelIndex + " StoryID------\n");
+        List<int> levelStoryList = GetLevelStoryID(levelIndex);
+        foreach (int storyID in levelStoryList)
+        {
+            print(storyID);
+        }
+    }
+
+    void PrintNextStoryID(int storyID, int storySeq, int fatherEventID)
+    {
+        print("\n-------StoryID  " + storyID +  ", StorySeq " + storySeq  + ", Before " + fatherEventID + ", Next Event" + "-----\n");
+        List<int> nextStoryList = GetNextStoryID(storyID, storySeq, fatherEventID);
+        foreach (int eventID in nextStoryList)
+        {
+            print(eventID);
+        }
+    }
+
+}
---
+++ Rothschild/Assets/Data/Script/PlayerDataProc.cs
@@ -0,0 +1,203 @@
+﻿using Mono.Xml;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Security;
+using System.Xml;
+using UnityEngine;
+
+
+struct PlayerData  //玩家信息
+{
+    public string name;
+    public string password;
+
+    public int level;
+    public int money;
+    public int reputation;  
+}
+
+public class PlayerDataProc : MonoBehaviour {
+
+    void CreatePlayerDB()
+    {
+        string path = Application.dataPath + "/Data/Xml/palyerDB.xml";
+        if (!File.Exists(path))
+        {
+            //创建最上一层的节点。
+            XmlDocument xml = new XmlDocument();
+            XmlElement root = xml.CreateElement("Players");
+            xml.AppendChild(root);
+            //最后保存文件
+            xml.Save(path);
+        }
+    }
+
+    void addPlayerData(PlayerData playerInfo)
+    {
+        string path = Application.dataPath + "/Data/Xml/palyerDB.xml";
+        if (File.Exists(path))
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(path);
+            XmlNode root = xml.SelectSingleNode("Players");
+            
+            XmlElement element = xml.CreateElement("Player");
+           
+            XmlElement elementName = xml.CreateElement("Name");
+            elementName.InnerText = playerInfo.name;
+
+            XmlElement elementPassword = xml.CreateElement("Password");
+            elementPassword.InnerText = playerInfo.password;
+
+            XmlElement elementLevel = xml.CreateElement("Level");
+            elementLevel.InnerText = playerInfo.level.ToString();
+
+            XmlElement elementMoney = xml.CreateElement("Money");
+            elementMoney.InnerText = playerInfo.money.ToString();
+
+            XmlElement elementReputation = xml.CreateElement("Reputation");
+            elementReputation.InnerText = playerInfo.reputation.ToString();
+
+            //把节点一层一层的添加至xml中，注意他们之间的先后顺序，这是生成XML文件的顺序
+            element.AppendChild(elementName);
+            element.AppendChild(elementPassword);
+            element.AppendChild(elementLevel);
+            element.AppendChild(elementMoney);
+            element.AppendChild(elementReputation);
+
+            root.AppendChild(element);
+
+            xml.AppendChild(root);
+            //最后保存文件
+            xml.Save(path);
+        }
+    }
+
+    // 更新玩家数据
+    void updatePlayerData(PlayerData playerInfo)
+    {
+        name = playerInfo.name;
+        string path = Application.dataPath + "/Data/Xml/palyerDB.xml";
+        if (File.Exists(path))
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(path);
+            XmlNodeList xmlNodeList = xml.SelectSingleNode("Players").ChildNodes;
+            foreach (XmlElement xl1 in xmlNodeList)
+            {
+                if (0 == name.CompareTo(xl1.ChildNodes[0].InnerText))
+                {
+                    xl1.ChildNodes[1].InnerText = playerInfo.password;
+                    xl1.ChildNodes[2].InnerText = playerInfo.level.ToString();
+                    xl1.ChildNodes[3].InnerText = playerInfo.money.ToString();
+                    xl1.ChildNodes[4].InnerText = playerInfo.reputation.ToString();
+
+                    break;
+                }
+            }
+            xml.Save(path);
+        }
+    }
+
+    bool FindPlayer(string name)
+    {
+        string path = Application.dataPath + "/Data/Xml/palyerDB.xml";
+        if (File.Exists(path))
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(path);
+            XmlNodeList xmlNodeList = xml.SelectSingleNode("Players").ChildNodes;
+            foreach (XmlElement xl1 in xmlNodeList)
+            {
+                if (0 == name.CompareTo(xl1.ChildNodes[0].InnerText))
+                {
+                    return true;   
+                }
+            }
+        }
+        return false;
+    }
+
+    int regPlayerData(string name, string password)
+    {
+        if (FindPlayer(name))
+        {
+            print("玩家已存在！！！");
+            return 1;
+        }
+        else
+        {
+            PlayerData playerInfo;
+
+            playerInfo.name = name;
+            playerInfo.password = password;
+            playerInfo.level = 1;
+            playerInfo.money = 0;
+            playerInfo.reputation = 0;
+
+            addPlayerData(playerInfo);
+        }
+        return 0;
+    }
+
+    int Login(ref PlayerData playerInfo)
+    {
+        string path = Application.dataPath + "/Data/Xml/palyerDB.xml";
+        string name = playerInfo.name;
+        string password = playerInfo.password;
+
+        if (File.Exists(path))
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(path);
+            XmlNodeList xmlNodeList = xml.SelectSingleNode("Players").ChildNodes;
+            foreach (XmlElement xl1 in xmlNodeList)
+            {
+                if (0 == name.CompareTo(xl1.ChildNodes[0].InnerText) && 
+                    0 == password.CompareTo(xl1.ChildNodes[1].InnerText))
+                {
+                    playerInfo.level = int.Parse(xl1.ChildNodes[2].InnerText);
+                    playerInfo.money = int.Parse(xl1.ChildNodes[3].InnerText);
+                    playerInfo.reputation = int.Parse(xl1.ChildNodes[4].InnerText);
+             
+                    return 0;
+                }
+            }
+        }
+
+        print("用户名或密码错误！！！");
+        return 1;
+    }
+
+    int Logout(PlayerData playerInfo)
+    {
+        updatePlayerData(playerInfo);
+        return 0;
+    }
+
+    // Use this for initialization
+    void Start () {
+        //CreatePlayerDB();
+        regPlayerData("neilxkchen", "43216");
+        PlayerData myInfo;
+        myInfo.name = "neilxkchen";
+        myInfo.password = "43216";
+        myInfo.level = 0;
+        myInfo.money = 0;
+        myInfo.reputation = 0;
+
+        Login(ref myInfo);
+
+        myInfo.level = 8888;
+
+        Logout(myInfo);
+
+     //   print("My Level: " + myInfo.level);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
