361dfc8b487d9e0bfe0a62b95933097fac6ba0a1
MaiJC <maijiancheng@qq.com>
2018-09-13 10:39:05 +0800

    加了一大堆关卡逻辑的代码，重写了一下UI响应的函数

--- Rothschild/Assets/Scripts/LevelManager.cs
+++ Rothschild/Assets/Scripts/LevelManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InterfaceManager : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
     LoadRes loadRes;
     private List<int> commonEventID = new List<int>();
@@ -12,8 +12,17 @@ public class InterfaceManager : MonoBehaviour
     private List<List<int>> levelStoryID = new List<List<int>>();
     private int currentEventID;
     private int currentLevel = 1;
+    private int currentEventCount = 0;
+    private int currentRound = 0;
+    private int currentMaxSelectedPersonCount = 2;
+    private int currentMaxSelectedChooseCount = 2;
+    private int currentSelectedCount = 0;
+    private int currentStoryHead;
+    private bool isInStory = false;
     private Dictionary<int, string> eventUIPath = new Dictionary<int, string>();
     private Dictionary<int, string> eventText = new Dictionary<int, string>();
+    private OnEvent onEvent;
+
 
     /*this is just use for monkeys*/
     private List<string> cardPath = new List<string>();
@@ -35,16 +44,17 @@ public class InterfaceManager : MonoBehaviour
 
     public void OnConfirm()
     {
-
+        NextEvent();
     }
 
     void Initialize()
     {
         loadRes = GameObject.Find("DataHandler").GetComponent<LoadRes>();
+        onEvent = GameObject.Find("EventSlot").GetComponent<OnEvent>();
 
         levelCount = loadRes.GetLevelCount();
         commonEventID = loadRes.GetCommonEventID();
-        for (int i = 1 ; i <= levelCount; i++)
+        for (int i = 1; i <= levelCount; i++)
         {
             levelEventID.Add(loadRes.GetLevelEventID(i));
             levelStoryID.Add(loadRes.GetLevelStoryID(i));
@@ -92,9 +102,79 @@ public class InterfaceManager : MonoBehaviour
         //}
     }
 
-    void GenerateCurrentEvent()
+    public bool AddSelect()
     {
-
+        if (currentSelectedCount == currentMaxSelectedPersonCount)
+        {
+            return false;
+        }
+        else
+        {
+            /*TODO: 增加如果选择数已达上限，则将未选择人物变灰的功能*/
+            currentSelectedCount++;
+            return true;
+        }
     }
+
+    public void RemoveSelect()
+    {
+        /*TODO: 增加如果原本选择数满上线，则将灰色的卡恢复彩色的功能s*/
+        currentSelectedCount--;
+        if (currentSelectedCount < 0)
+            Debug.LogError("currentSelectedCount less than 0!");
+    }
+
+    void NextEvent()
+    {
+        /*TODO: 增加是否为下一关的判断*/
+
+        currentRound++;
+
+        if(isInStory)
+        {
+            //currentEventID=loadRes.GetNextStoryID(currentStoryHead,)
+        }
+        else
+        {
+            /*TODO: 目前故事的出现是完全随机，没有限制回合数的，后期需要加入故事出现的回合数判断*/
+            //从事件池里面随机事件
+            int idx = Random.Range(0, currentEventCount + levelStoryID[currentLevel - 1].Count);
+            if (idx < commonEventID.Count)
+            {
+                currentEventID = commonEventID[idx];
+                commonEventID.RemoveAt(idx);
+                currentEventCount--;
+            }
+            else if (idx < currentEventCount)
+            {
+                currentEventID = levelEventID[currentLevel - 1][idx - commonEventID.Count];
+                commonEventID.RemoveAt(idx - commonEventID.Count);
+                currentEventCount--;
+            }
+            else
+            {
+                currentEventID = levelStoryID[currentLevel - 1][idx - currentEventCount];
+                levelStoryID.RemoveAt(idx - currentEventCount);
+                isInStory = true;
+                currentStoryHead = currentEventID;
+            }
+        }
+        
+        
+
+        /*TODO: 添加事件切换特效*/
+        //为事件槽设置新的图片和文字描述
+        onEvent.SetImage(eventUIPath[currentEventID]);
+        onEvent.SetText(eventText[currentEventID]);
+    }
+
+    void NextLevel()
+    {
+        currentLevel++;
+
+        currentEventCount = commonEventID.Count + levelEventID[currentLevel - 1].Count;
+    }
+
+
 
 }
---
+++ Rothschild/Assets/Scripts/OnEvent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnEvent : MonoBehaviour {
+
+    private Image eventImage;
+    private Text eventText;
+    private int eventID;
+    private LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+        eventImage = this.transform.GetChild(0).gameObject.GetComponent<Image>();
+        eventText = this.transform.GetChild(1).gameObject.GetComponent<Text>();
+        levelManager = GameObject.Find("LogicHandler").GetComponent<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetImage(string path)
+    {
+        eventImage.sprite = Resources.Load(path, typeof(Sprite)) as Sprite;
+    }
+
+    public void SetText(string text)
+    {
+        eventText.text = text;
+    }
+
+    private void SetID(int id)
+    {
+        eventID = id;
+    }
+
+}
